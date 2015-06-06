b0ad64330359e02145ae78fc61f14a9207986857
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-06-06 11:08:19 +0200

    added requirement for windows operating system adjustes readme

--- terminal_vis.rb
+++ terminal_vis.rb
@@ -1,12 +1,18 @@
 # @Author: Benjamin Held
 # @Date:   2015-05-31 14:25:27
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-06-05 14:21:03
+# @Last Modified time: 2015-06-06 11:07:34
 
 require_relative './graphics/string'
 require_relative './graphics/color_legend'
 require_relative './data/data_repository'
 require_relative './output/output'
+
+begin
+  require 'Win32/Console/ANSI' if RUBY_PLATFORM =~ /win32/
+rescue LoadError
+  raise 'You must gem install win32console to use color on Windows'
+end
 
 if (ARGV.length != 1)
     STDERR.puts "Invalid number of arguments: usage ruby <script> <filename>"
