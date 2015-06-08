5d826d6e4604b319dce7fef302e179a6ca4ad483
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-06-08 21:18:19 +0200

    started rdoc

--- terminal_vis/lib/output/output.rb
+++ terminal_vis/lib/output/output.rb
@@ -1,18 +1,21 @@
 # @Author: Benjamin Held
 # @Date:   2015-05-31 15:08:28
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-06-04 09:36:41
+# @Last Modified time: 2015-06-08 21:13:00
 
 require_relative '../data/data_set'
 require_relative '../graphics/color_legend'
 
+# Simple output for the terminal visualization
 class Output
     attr_reader :legend
 
+    # Initialization with data set that should be visualized
     def initialize(data_set)
         @legend = ColorLegend.new(data_set.min_value, data_set.max_value)
     end
 
+    # Reversed the data to print it in the correct occurence
     def print_data(data_set)
         reversed_data = data_set.data.to_a.reverse.to_h
         reversed_data.each_value { |row|
