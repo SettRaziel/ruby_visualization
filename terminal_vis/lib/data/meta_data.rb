ee06739b5ec3d29b85df0cbc856b9c60ed4dba03
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-06-16 17:11:12 +0200

    added distinction between two dimensional and three dimensional data sets adjusted meta data to precess the data sets updated readme

--- terminal_vis/lib/data/meta_data.rb
+++ terminal_vis/lib/data/meta_data.rb
@@ -1,26 +1,42 @@
 # @Author: Benjamin Held
 # @Date:   2015-06-09 12:49:43
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-06-14 09:42:28
+# @Last Modified time: 2015-06-16 17:08:15
 
+# MetaData stores meta information about the data set
+# two dimensional data set =>
+# <data_name>,
+# <axis_description_x>,<lower_boundary_x>,<upper_boundary_x>,
+# <axis_description_y>,<lower_boundary_y>,<upper_boundary_y>
+#
+# three dimensional data set =>
+# <data_name>,
+# <axis_description_x>,<lower_boundary_x>,<upper_boundary_x>,
+# <axis_description_y>,<lower_boundary_y>,<upper_boundary_y>,
+# <axis_description_z>,<lower_boundary_z>,<upper_boundary_z>
 class MetaData
     attr_reader :name, :domain_x, :domain_y, :domain_z
 
     def initialize(metadata)
 
-        if metadata.length != 12
+        if (metadata.length != 13 || metadata.length != 9)
             STDERR.puts "Error in meta data: incorrect number of arguments:" \
             " #{metadata.length}."
             exit(0)
         end
 
-        @name = metadata[8]
-        @domain_x = DataDomain.new(metadata[0], metadata[1], \
-                                   metadata[2], metadata[3])
-        @domain_y = DataDomain.new(metadata[4], metadata[5], \
-                                   metadata[6], metadata[7])
-        @domain_z = DataDomain.new(metadata[8], metadata[9], \
-                                   metadata[10], metadata[11])
+        @name = metadata[0]
+        @domain_x = DataDomain.new(metadata[1], metadata[2], \
+                                   metadata[3], metadata[4])
+        @domain_y = DataDomain.new(metadata[5], metadata[6], \
+                                   metadata[7], metadata[8])
+
+        if (metadata.length == 13)
+            @domain_z = DataDomain.new(metadata[9], metadata[10], \
+                                       metadata[11], metadata[12])
+        else
+            @domain_z = nil
+        end
     end
 end
 
