4648e4c3ce75ecbe1cdfb79b3f130b26aad6b436
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-07-16 12:33:14 +0200

    refactored output moved prints of meta informations to seperate method changed the sequence of the dimension output to x, y, z

--- terminal_vis/lib/output/output.rb
+++ terminal_vis/lib/output/output.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-05-31 15:08:28
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-07-15 08:22:36
+# @Last Modified time: 2015-07-16 12:31:08
 
 require_relative '../data/data_set'
 require_relative '../graphics/color_legend'
@@ -21,6 +21,8 @@ class Output
 
         print_output_head(index, meta_data)
 
+        # reverse the data to start with the highest y-value as the first
+        # output line
         reversed_data = data_set.data.to_a.reverse.to_h
 
         reversed_data.each_value { |row|
@@ -32,26 +34,33 @@ class Output
         puts ""
         legend.print_color_legend()
 
+        print_meta_information(meta_data)
+
+        puts "\n"
+    end
+
+    private
+
+    # prints the meta information consisting of dataset name and informations
+    # of the different dimensions
+    def print_meta_information(meta_data)
         puts "\nDataset: #{meta_data.name}"
 
-        print "\nY-axis with #{meta_data.domain_y.name} from " \
-             "#{meta_data.domain_y.lower}"
-        puts " up to #{meta_data.domain_y.upper} and steprange" \
-             " #{meta_data.domain_y.step}."
-        print "X-axis with #{meta_data.domain_x.name} from " \
+        print "\nX-axis with #{meta_data.domain_x.name} from " \
               "#{meta_data.domain_x.lower}"
         puts " up to #{meta_data.domain_x.upper} and steprange" \
              " #{meta_data.domain_x.step}."
+        print "Y-axis with #{meta_data.domain_y.name} from " \
+             "#{meta_data.domain_y.lower}"
+        puts " up to #{meta_data.domain_y.upper} and steprange" \
+             " #{meta_data.domain_y.step}."
         if (meta_data.domain_z != nil)
             print "Z-axis with #{meta_data.domain_z.name} from " \
               "#{meta_data.domain_z.lower}"
             puts " up to #{meta_data.domain_z.upper} and steprange" \
                  " #{meta_data.domain_z.step}."
         end
-        puts "\n"
     end
-
-    private
 
     # creates a headline before printing the data set based on the values
     # of the z dimension
