79cc0c1717718f7ade34b66171937242cebf28e6
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-07-26 13:35:42 +0200

    extended help output added method to print help text for a given parameter specified help text to the available parameters

--- terminal_vis/lib/output/help_output.rb
+++ terminal_vis/lib/output/help_output.rb
@@ -1,23 +1,53 @@
 # @Author: Benjamin Held
 # @Date:   2015-07-25 12:17:16
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-07-25 12:28:41
+# @Last Modified time: 2015-07-26 13:35:17
 
 # Output class for help text
 class HelpOutput
 
-    # methode to print the default help text
+    # method to print the default help text
     def self.print_help
         puts "usage: ruby <script> [parameters] <filename>"
         puts "\nTerminalVis help:"
         puts " -h, --help     show help text"
         puts " -v, --version  prints the current version of the project"
         puts " -m             process the file <filename> containing meta data"
-        puts " -i <index>     shows the dataset at index, if index lies within " \
-                              "[1,2, ..., number of datasets], excludes -a, --all"
-        puts " -a, --all      prints all possible datasets of a dataseries with " \
-                              "a pause between the output of every dataset, " \
-                              "excludes -i."
+        puts " -i <index>     shows the dataset at index, if index lies " \
+             "within [1,2, ..., number of datasets], excludes -a, --all"
+        puts " -a, --all      prints all possible datasets of a dataseries " \
+             "with a pause between the output of every dataset, excludes -i."
     end
+
+    # method to print the help text for the given parameter
+    # raises ArgumentError when a non existent parameter is provided
+    def self.print_help_for(parameter)
+        if (@parameters[parameter])
+            puts @parameters[parameter]
+        else
+            raise ArgumentError, "help entry for #{parameter} does not exist"
+        end
+    end
+
+    private
+
+    # hash which stores available paramaeters and their help text
+    @parameters = {
+        '-h' => ' -h, --help     show help text',
+        '--help' => ' -h, --help     show help text',
+        '-v' => ' -v, --version  prints the current version of the project',
+        '--version' => ' -v, --version  prints the current version of the' \
+                       ' project',
+        '-m' => ' -m             process the file <filename> containing' \
+                ' meta data',
+        '-i' => ' -i <index>     shows the dataset at index, if index lies ' \
+                'within [1,2, ..., number of datasets], excludes -a, --all',
+        '-a' => ' -a, --all      prints all possible datasets of a ' \
+                ' dataseries with a pause between the output of every ' \
+                'dataset, excludes -i',
+        '-all' => ' -a, --all      prints all possible datasets of a ' \
+                ' dataseries with a pause between the output of every ' \
+                'dataset, excludes -i'
+    }
 
 end
