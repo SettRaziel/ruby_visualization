5f37f6d0809c9d21c9c7083070c79f885b117823
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2015-07-30 09:19:43 +0200

    extended string class added methods for additional terminal colors and backgrounds

--- terminal_vis/lib/graphics/string.rb
+++ terminal_vis/lib/graphics/string.rb
@@ -1,124 +1,200 @@
 # @Author: Benjamin Held
 # @Date:   2015-05-30 08:57:40
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-07-29 09:02:36
+# @Last Modified time: 2015-07-30 08:58:38
 
 # Class to modify a String in color and typo
+# \e stands for the esc code
 class String
 
     # String => colored bright
     def bright
-        "\033[1m#{self}\033[0m"
+        "\e[1m#{self}\e[0m"
     end
 
     # String => colored gray
     def gray
-        "\033[2m#{self}\033[0m"
+        "\e[2m#{self}\e[0m"
     end
 
     # String => formatted in italic
     def italic
-        "\033[3m#{self}\033[0m"
+        "\e[3m#{self}\e[0m"
     end
 
     # String => formatted as underlined String
     def underline
-        "\033[4m#{self}\033[0m"
+        "\e[4m#{self}\e[0m"
     end
 
     # String => colored blink
     def blink
-        "\033[5m#{self}\033[0m"
+        "\e[5m#{self}\e[0m"
     end
 
-    # String => exchanged grounds
+    # String => exchanged color and background
     def exchange_grounds
-        "\033[7m#{self}\033[0m"
+        "\e[7m#{self}\e[0m"
     end
 
     # String => colored with background color
     def hide
-        "\033[8m#{self}\033[0m"
+        "\e[8m#{self}\e[0m"
     end
 
     # String => colored black
     def black
-        "\033[30m#{self}\033[0m"
+        "\e[30m#{self}\e[0m"
     end
 
     # String => colored red
     def red
-        "\033[31m#{self}\033[0m"
+        "\e[31m#{self}\e[0m"
+    end
+
+    # String => colored light_red
+    def light_red
+        "\e[91m#{self}\e[0m"
     end
 
     # String => colored green
     def green
-        "\033[32m#{self}\033[0m"
+        "\e[32m#{self}\e[0m"
+    end
+
+    # String => colored light_green
+    def light_green
+        "\e[92m#{self}\e[0m"
     end
 
     # String => colored yellow
     def yellow
-        "\033[33m#{self}\033[0m"
+        "\e[33m#{self}\e[0m"
+    end
+
+    # String => colored light_yellow
+    def light_yellow
+        "\e[93m#{self}\e[0m"
     end
 
     # String => colored blue
     def blue
-        "\033[34m#{self}\033[0m"
+        "\e[34m#{self}\e[0m"
+    end
+
+    # String => colored light_blue
+    def light_blue
+        "\e[94m#{self}\e[0m"
     end
 
     # String => colored magenta
     def magenta
-        "\033[35m#{self}\033[0m"
+        "\e[35m#{self}\e[0m"
+    end
+
+    # String => colored light_magenta
+    def light_magenta
+        "\e[95m#{self}\e[0m"
     end
 
     # String => colored cyan
     def cyan
-        "\033[36m#{self}\033[0m"
+        "\e[36m#{self}\e[0m"
+    end
+
+    # String => colored light_cyan
+    def light_cyan
+        "\e[96m#{self}\e[0m"
+    end
+
+    # String => colored light_gray
+    def light_gray
+        "\e[37m#{self}\e[0m"
     end
 
     # String => colored white
     def white
-        "\033[37m#{self}\033[0m"
+        "\e[97m#{self}\e[0m"
     end
 
     # String => background colored black
     def black_bg
-        "\033[40m#{self}\033[0m"
+        "\e[40m#{self}\e[0m"
     end
 
     # String => background colored red
     def red_bg
-        "\033[41m#{self}\033[0m"
+        "\e[41m#{self}\e[0m"
+    end
+
+    # String => background colored light_red
+    def light_red_bg
+        "\e[101m#{self}\e[0m"
     end
 
     # String => background colored green
     def green_bg
-        "\033[42m#{self}\033[0m"
+        "\e[42m#{self}\e[0m"
+    end
+
+    # String => background colored light_green
+    def light_green_bg
+        "\e[102m#{self}\e[0m"
     end
 
     # String => background colored yellow
     def yellow_bg
-        "\033[43m#{self}\033[0m"
+        "\e[43m#{self}\e[0m"
+    end
+
+    # String => background colored light_yellow
+    def light_yellow_bg
+        "\e[103m#{self}\e[0m"
     end
 
     # String => background colored blue
     def blue_bg
-        "\033[44m#{self}\033[0m"
+        "\e[44m#{self}\e[0m"
+    end
+
+    # String => background colored light_blue
+    def light_blue_bg
+        "\e[104m#{self}\e[0m"
     end
 
     # String => background colored magenta
     def magenta_bg
-        "\033[45m#{self}\033[0m"
+        "\e[45m#{self}\e[0m"
+    end
+
+    # String => background colored light_magenta
+    def light_magenta_bg
+        "\e[105m#{self}\e[0m"
     end
 
     # String => background colored cyan
     def cyan_bg
-        "\033[46m#{self}\033[0m"
+        "\e[46m#{self}\e[0m"
+    end
+
+    # String => background colored light_cyan
+    def light_cyan_bg
+        "\e[106m#{self}\e[0m"
     end
 
     # String => background colored white
     def white_bg
-        "\033[47m#{self}\033[0m"
+        "\e[107m#{self}\e[0m"
+    end
+
+    # String => background colored light_gray
+    def light_gray_bg
+        "\e[47m#{self}\e[0m"
+    end
+
+    # String => background colored dark_gray
+    def dark_gray_bg
+        "\e[100m#{self}\e[0m"
     end
 
 end
