cb8c52aae379984640a24ab121516197cac732ba
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-01-09 10:27:31 +0100

    bump to 0.8.0

--- terminal_vis/bin/terminal_vis.rb
+++ terminal_vis/bin/terminal_vis.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-05-31 14:25:27
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-01-06 09:13:45
+# @Last Modified time: 2016-01-09 10:27:04
 
 
 require_relative '../lib/data/data_repository'
@@ -10,7 +10,7 @@ require_relative '../lib/main/main_module'
 
 #-------------------------------------------------------------------------------
 # Terminal Visualization Script
-# Version 0.7.2
+# Version 0.8.0
 # created by Benjamin Held, June 2015
 begin
   require 'Win32/Console/ANSI' if RUBY_PLATFORM =~ /win32/
--- terminal_vis/lib/main/main_module.rb
+++ terminal_vis/lib/main/main_module.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-20 08:40:28
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-12-07 17:47:01
+# @Last Modified time: 2016-01-09 10:25:16
 
 module TerminalVis
 
@@ -60,7 +60,7 @@ module TerminalVis
 
   # call to print version number and author
   def self.print_version
-    puts 'terminal_visualization version 0.7.2'
+    puts 'terminal_visualization version 0.8.0'
     puts 'Created by Benjamin Held (June 2015)'
     exit(0)
   end
