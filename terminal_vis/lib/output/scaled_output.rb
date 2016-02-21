094f71ef3bf342cca692716d1f62a18e9f9cbfc7
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-02-21 17:14:14 +0100

    adjusted scaled_output - added it as child of dataset_output to reuse output method - used the meta_data of the parent class for the adjusted meta informations - adjusted the code structure

--- terminal_vis/lib/output/scaled_output.rb
+++ terminal_vis/lib/output/scaled_output.rb
@@ -1,11 +1,11 @@
 # @Author: Benjamin Held
 # @Date:   2016-01-29 10:17:38
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-02-19 14:15:30
+# @Last Modified time: 2016-02-21 17:08:49
 
 module DataOutput
 
-  class ScaledOutput < Base
+  class ScaledOutput < DatasetOutput
 
     require_relative '../main/main_module'
     require_relative '../configuration/terminal_size'
@@ -20,12 +20,10 @@ module DataOutput
       prepare_attributes(data_set, meta_data, options)
       print_output_head(options[:index], meta_data)
       # call print_data of the the parent class
-      print_data(options[:legend], @scale_meta.domain_x, @scale_meta.domain_y)
+      print_data(options[:legend], @meta_data.domain_x, @meta_data.domain_y)
     end
 
     private
-    # @return [MetaData] the meta data for this output
-    attr :scale_meta
     # @return [Integer] the number of lines of the used terminal
     attr :lines
     # @return [Integer] the number of fields per row of the used terminal
@@ -37,7 +35,7 @@ module DataOutput
     # @param [Hash] options hash with the relevant parameter values
     def self.prepare_attributes(data_set, meta_data, options)
       # create meta data and data set for the scaled output
-      @scale_meta = calculate_resolution_meta(meta_data)
+      @meta_data = calculate_resolution_meta(meta_data)
       scaled_data_set = calculate_scaled_dataset(data_set, meta_data)
 
       @legend = ColorLegend::ColorData.new(scaled_data_set.min_value,
@@ -54,19 +52,6 @@ module DataOutput
       puts "\n"
     end
 
-    # prints the meta information consisting of dataset name and informations
-    # of the different dimensions
-    def self.print_meta_information
-      puts "\nDataset: #{@scale_meta.name}"
-
-      print_domain_information(@scale_meta.domain_x, "\nX")
-      print_domain_information(@scale_meta.domain_y, 'Y')
-
-      if (@scale_meta.domain_z != nil)
-        print_domain_information(@scale_meta.domain_z, 'Z')
-      end
-    end
-
     # method to retrieve the current dimension of the used terminal
     def self.set_terminal_dimension
       ts = TerminalSize::TerminalSize.new()
@@ -79,19 +64,26 @@ module DataOutput
     # @param [MetaData] meta_data the corresponding meta data
     # @return [MetaData] the meta data of the scaled dataset
     def self.calculate_resolution_meta(meta_data)
+      # determine new meta information and create ne meta object
+      meta_string = [meta_data.name]
+      meta_string.concat(create_new_data_dimensions(meta_data))
+      meta_string.concat(add_domain_information(meta_data.domain_z,
+                                                meta_data.domain_z.step))
+      # return the new meta data object
+      MetaData::MetaData.new(meta_string)
+    end
+
+    # method to create the entris for the x and y domain of the scaled meta data
+    # @param [MetaData] meta_data the corresponding meta data
+    # @return [Array] the meta informations for the x and y domain
+    def self.create_new_data_dimensions(meta_data)
       # map the meta information to the resolution
       delta_x = ((meta_data.domain_x.upper - meta_data.domain_x.lower) /
                  @columns).round(3) * 2
       delta_y = ((meta_data.domain_y.upper - meta_data.domain_y.lower) /
                  @lines).round(3)
-      # determine new meta information and create ne meta object
-      meta_string = [meta_data.name]
-      meta_string.concat(add_domain_information(meta_data.domain_x, delta_x))
+      meta_string = add_domain_information(meta_data.domain_x, delta_x)
       meta_string.concat(add_domain_information(meta_data.domain_y, delta_y))
-      meta_string.concat(add_domain_information(meta_data.domain_z,
-                                                meta_data.domain_z.step))
-      # return the new meta data object
-      MetaData::MetaData.new(meta_string)
     end
 
     # method to check if the dimensions of the terminal are big enough to
@@ -125,20 +117,20 @@ module DataOutput
     # @return [Hash] a hash with the x and y coordinate
     def self.determine_coordinates
       coordinates = Hash.new()
-      coordinates[:x] = @scale_meta.domain_x.lower +
-                        calculated_dimension_delta(@scale_meta.domain_x)
-      coordinates[:y] = @scale_meta.domain_y.lower +
-                        calculated_dimension_delta(@scale_meta.domain_y)
+      coordinates[:x] = @meta_data.domain_x.lower +
+                        calculated_dimension_delta(@meta_data.domain_x)
+      coordinates[:y] = @meta_data.domain_y.lower +
+                        calculated_dimension_delta(@meta_data.domain_y)
       return coordinates
     end
 
     # method to calculate the required parameter values for the interpolation
     # @return [Hash] a hash with the required parameters
     def self.determine_values
-      { :inter_x => calculated_dimension_delta(@scale_meta.domain_x),
-        :delta_x => @scale_meta.domain_x.step,
-        :inter_y => calculated_dimension_delta(@scale_meta.domain_y),
-        :delta_y => @scale_meta.domain_y.step }
+      { :inter_x => calculated_dimension_delta(@meta_data.domain_x),
+        :delta_x => @meta_data.domain_x.step,
+        :inter_y => calculated_dimension_delta(@meta_data.domain_y),
+        :delta_y => @meta_data.domain_y.step }
     end
 
     # method to calculate the half distance of a data domain
