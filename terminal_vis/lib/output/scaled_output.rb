15555192cc7b1e892c77ccc3283109cf1b0a4ece
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-03-12 12:14:30 +0100

    moved the creation of a scaled dataset to a seperate class - added class dataset_scaling - removed scaling from scaled_output - adjusted code to use the new class

--- terminal_vis/lib/output/scaled_output.rb
+++ terminal_vis/lib/output/scaled_output.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-01-29 10:17:38
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-03-10 11:47:47
+# @Last Modified time: 2016-03-12 12:02:36
 
 module DataOutput
 
@@ -11,6 +11,7 @@ module DataOutput
 
     require_relative '../main/main_module'
     require_relative '../scaling/terminal_size'
+    require_relative '../scaling/dataset_scaling'
 
     # method to print a given dataset scaled by the terminal size
     # @param [DataSet] data_set the data set which should be visualized
@@ -18,7 +19,6 @@ module DataOutput
     # @param [Hash] options hash with the relevant parameter values
     def self.print_dataset(data_set, meta_data, options)
       # read terminal size from options hash
-      set_terminal_dimension
       prepare_attributes(data_set, meta_data, options)
       print_output_head(options[:index], meta_data)
       # call print_data of the the parent class
@@ -26,10 +26,6 @@ module DataOutput
     end
 
     private
-    # @return [Integer] the number of lines of the used terminal
-    attr :lines
-    # @return [Integer] the number of fields per row of the used terminal
-    attr :columns
 
     # method to set the required attributes und create the scaled dataset
     # @param [DataSet] data_set the data set which should be visualized
@@ -37,8 +33,9 @@ module DataOutput
     # @param [Hash] options hash with the relevant parameter values
     def self.prepare_attributes(data_set, meta_data, options)
       # create meta data and data set for the scaled output
-      @meta_data = calculate_resolution_meta(meta_data)
-      scaled_data_set = calculate_scaled_dataset(data_set, meta_data)
+      sc = DatasetScaling.new(meta_data)
+      @meta_data = sc.scaled_meta
+      scaled_data_set = sc.calculate_scaled_dataset(data_set)
 
       @legend = ColorLegend::ColorData.new(scaled_data_set.min_value,
                                            scaled_data_set.max_value)
@@ -52,93 +49,6 @@ module DataOutput
       puts "\nPrinting autoscaled dataset for %.2f" %
             (meta_data.domain_z.lower + (index * meta_data.domain_z.step))
       puts "\n"
-    end
-
-    # method to retrieve the current dimension of the used terminal
-    def self.set_terminal_dimension
-      ts = TerminalSize::TerminalSize.new()
-      @lines = ts.lines - 14
-      @columns = ts.columns - 10
-      check_value_boundaries
-    end
-
-    # method to create the meta data object for the scaled dataset
-    # @param [MetaData] meta_data the corresponding meta data
-    # @return [MetaData] the meta data of the scaled dataset
-    def self.calculate_resolution_meta(meta_data)
-      # determine new meta information and create ne meta object
-      meta_string = [meta_data.name]
-      meta_string.concat(create_new_data_dimensions(meta_data))
-      meta_string.concat(add_domain_information(meta_data.domain_z,
-                                                meta_data.domain_z.step))
-      # return the new meta data object
-      MetaData::MetaData.new(meta_string)
-    end
-
-    # method to create the entris for the x and y domain of the scaled meta data
-    # @param [MetaData] meta_data the corresponding meta data
-    # @return [Array] the meta informations for the x and y domain
-    def self.create_new_data_dimensions(meta_data)
-      # map the meta information to the resolution
-      delta_x = ((meta_data.domain_x.upper - meta_data.domain_x.lower) /
-                 @columns).round(3) * 2
-      delta_y = ((meta_data.domain_y.upper - meta_data.domain_y.lower) /
-                 @lines).round(3)
-      meta_string = add_domain_information(meta_data.domain_x, delta_x)
-      meta_string.concat(add_domain_information(meta_data.domain_y, delta_y))
-    end
-
-    # method to check if the dimensions of the terminal are big enough to
-    # create a useful result
-    def self.check_value_boundaries
-      if (@lines < 9 || @columns < 20)
-        raise ArgumentError,
-              ' Error: The terminal size is to small for scaled output'.red
-      end
-    end
-
-    # method to add the required parameter to the meta data string
-    # @return [Array] an array containing the string values for the given domain
-    def self.add_domain_information(data_domain, new_step)
-      [data_domain.name, data_domain.lower, data_domain.upper, new_step]
-    end
-
-    # method to create the scaled dataset by interpolating new data values
-    # @param [DataSet] data_set the data set which should be visualized
-    # @param [MetaData] meta_data the corresponding meta data
-    # @return [Dataset] the scaled dataset
-    def self.calculate_scaled_dataset(data_set, meta_data)
-      coordinates = determine_coordinates
-      values = determine_values
-
-      TerminalVis::Interpolation.region_interpolation(meta_data,
-                                 data_set, coordinates, values)
-    end
-
-    # method to retrieve the central coordinate of the dataset
-    # @return [Hash] a hash with the x and y coordinate
-    def self.determine_coordinates
-      coordinates = Hash.new()
-      coordinates[:x] = @meta_data.domain_x.lower +
-                        calculated_dimension_delta(@meta_data.domain_x)
-      coordinates[:y] = @meta_data.domain_y.lower +
-                        calculated_dimension_delta(@meta_data.domain_y)
-      return coordinates
-    end
-
-    # method to calculate the required parameter values for the interpolation
-    # @return [Hash] a hash with the required parameters
-    def self.determine_values
-      { :inter_x => calculated_dimension_delta(@meta_data.domain_x),
-        :delta_x => @meta_data.domain_x.step,
-        :inter_y => calculated_dimension_delta(@meta_data.domain_y),
-        :delta_y => @meta_data.domain_y.step }
-    end
-
-    # method to calculate the half distance of a data domain
-    # @return [Float] the middle distance of the given data domain
-    def self.calculated_dimension_delta(data_domain)
-      ((data_domain.upper - data_domain.lower) / 2).round(3)
     end
 
   end
---
+++ terminal_vis/lib/scaling/dataset_scaling.rb
@@ -0,0 +1,116 @@
+# @Author: Benjamin Held
+# @Date:   2016-03-10 11:45:32
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2016-03-12 12:12:59
+
+require_relative '../data/meta_data'
+require_relative 'terminal_size'
+
+# Helper class to generate a scaled {DataSet} from a {MetaData} => {DataSet}
+# pair with the scaling based on the size of the terminal where the script is
+# started.
+class DatasetScaling
+  # @return [MetaData] the {MetaData} of the scaled {DataSet}
+  attr_reader :scaled_meta
+
+  # initialization
+  # @param [MetaData] meta_data the {MetaData} of the {DataSet} that should
+  #    be scaled
+  def initialize(meta_data)
+    @meta_data = meta_data
+    ts = TerminalSize::TerminalSize.new()
+    @lines = ts.lines - 14
+    @columns = ts.columns - 10
+    check_value_boundaries
+    calculate_new_resolution_meta
+  end
+
+  # method to create the scaled dataset by interpolating new data values
+  # @param [DataSet] data_set the data set which should be visualized
+  # @return [Dataset] the scaled dataset
+  def calculate_scaled_dataset(data_set)
+    coordinates = determine_coordinates
+    values = determine_values
+
+    TerminalVis::Interpolation.region_interpolation(@meta_data, data_set,
+                                                    coordinates, values)
+  end
+
+  private
+  # @return [MetaData] the original {MetaData} of the {DataSet}
+  attr :meta_data
+  # @return [Integer] the number of lines of the used terminal
+  attr :lines
+  # @return [Integer] the number of fields per row of the used terminal
+  attr :columns
+
+  # method to create the meta data object for the scaled dataset
+  def calculate_new_resolution_meta
+    # determine new meta information and create ne meta object
+    meta_string = [@meta_data.name]
+    meta_string.concat(create_new_data_dimensions)
+    meta_string.concat(add_domain_information(@meta_data.domain_z,
+                                              @meta_data.domain_z.step))
+    # return the new meta data object
+    @scaled_meta = MetaData::MetaData.new(meta_string)
+  end
+
+  # method to check if the dimensions of the terminal are big enough to
+  # create a useful result
+  def check_value_boundaries
+    if (@lines < 9 || @columns < 20)
+      raise ArgumentError,
+            ' Error: The terminal size is to small for scaled output'.red
+    end
+  end
+
+  # method to create the entris for the x and y domain of the scaled meta data
+  # @return [Array] the meta informations for the x and y domain
+  def create_new_data_dimensions
+    # map the meta information to the resolution
+    delta_x = ((@meta_data.domain_x.upper - @meta_data.domain_x.lower) /
+               @columns).round(3) * 2
+    delta_y = ((@meta_data.domain_y.upper - @meta_data.domain_y.lower) /
+               @lines).round(3)
+    meta_string = add_domain_information(@meta_data.domain_x, delta_x)
+    meta_string.concat(add_domain_information(@meta_data.domain_y, delta_y))
+  end
+
+  # method to add the required parameter to the meta data string
+  # @param [DataDomain] data_domain the required {DataDomain}
+  # @param [Float] new_step the new delta between two data values for the given
+  #    data dimension
+  # @return [Array] an array containing the string values for the given domain
+  def add_domain_information(data_domain, new_step)
+    [data_domain.name, data_domain.lower, data_domain.upper, new_step]
+  end
+
+
+  # method to retrieve the central coordinate of the dataset
+  # @return [Hash] a hash with the x and y coordinate
+  def determine_coordinates
+    coordinates = Hash.new()
+    coordinates[:x] = @scaled_meta.domain_x.lower +
+                      calculated_dimension_delta(@scaled_meta.domain_x)
+    coordinates[:y] = @scaled_meta.domain_y.lower +
+                      calculated_dimension_delta(@scaled_meta.domain_y)
+    return coordinates
+  end
+
+  # method to calculate the required parameter values for the interpolation
+  # @return [Hash] a hash with the required parameters
+  def determine_values
+    { :inter_x => calculated_dimension_delta(@scaled_meta.domain_x),
+      :delta_x => @scaled_meta.domain_x.step,
+      :inter_y => calculated_dimension_delta(@scaled_meta.domain_y),
+      :delta_y => @scaled_meta.domain_y.step }
+  end
+
+  # method to calculate the half distance of a data domain
+  # @param [DataDomain] data_domain the required {DataDomain}
+  # @return [Float] the middle distance of the given data domain
+  def calculated_dimension_delta(data_domain)
+    ((data_domain.upper - data_domain.lower) / 2).round(3)
+  end
+
+end
