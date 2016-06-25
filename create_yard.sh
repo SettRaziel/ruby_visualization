6df9028666718bc7f4d94b451807755499254163
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-25 14:41:40 +0200

    adjusted project structure - removed intermediate folder - adjusted gitignore - adjusted documentation shell script

--- create_yard.sh
+++ create_yard.sh
@@ -2,6 +2,6 @@
 # @Author: Benjamin Held
 # @Date:   2015-08-30 09:48:37
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2015-09-21 13:14:55
+# @Last Modified time: 2016-06-24 17:46:15
 
-exec yard doc --private --readme ../README.md
+exec yard doc --private --readme ./README.md
