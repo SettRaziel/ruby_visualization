1627e20dd24066c87664632d787067148e2b0257
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-10-08 09:04:45 +0200

    adjusted documentation

--- lib/scaling/terminal_size.rb
+++ lib/scaling/terminal_size.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-01-18 13:03:35
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-03-08 14:28:39
+# @Last Modified time: 2016-10-08 09:01:17
 
 # module for classes and methods that determine the actual dimension of the
 # terminal executing the script
@@ -56,9 +56,10 @@ module TerminalSize
     end
 
     # method to check if the given command can be executed
+    # @param [String] command the provided command string
     # @return [Boolean] true: command can be used, false if not
     def command_exists?(command)
-      ENV['PATH'].split(File::PATH_SEPARATOR).any? {|d|
+      ENV['PATH'].split(File::PATH_SEPARATOR).any? { |d|
         File.exist?(File.join(d, command))
       }
     end
