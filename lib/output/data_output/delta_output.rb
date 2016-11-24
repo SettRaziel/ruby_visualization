658e2f3b5322b39c0cea6b59326b0104214561fe
Benjamin Held <me@bheld.eu>
2016-11-24 20:52:03 +0100

    cont. refactoring delta_output

--- lib/output/data_output/delta_output.rb
+++ lib/output/data_output/delta_output.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-01-12 09:30:35
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-06-29 18:06:42
+# @Last Modified time: 2016-11-24 20:51:03
 
 module DataOutput
 
@@ -16,7 +16,7 @@ module DataOutput
     #   substracted
     # @param [Hash] options hash with the boolean values for extreme values and
     #   extended legend output
-    def self.print_delta(data_set, meta_data, indices, options)
+    def initialize(data_set, meta_data, indices, options)
       @meta_data = meta_data
       set_attributes(data_set, options[:extreme_values])
       @legend = ColorLegend::ColorDelta.
@@ -24,6 +24,7 @@ module DataOutput
 
       print_output_head(indices)
       print_data(options[:legend], @meta_data.domain_x, @meta_data.domain_y)
+      nil
     end
 
     private
@@ -32,17 +33,19 @@ module DataOutput
     # indices
     # @param [Hash] indices the indices of the two datasets which should be
     #   substracted
-    def self.print_output_head(indices)
+    def print_output_head(indices)
       first_year = @meta_data.domain_z.lower + indices[:first]
       second_year = @meta_data.domain_z.lower + indices[:second]
       puts "Printing difference for the datasets of #{first_year} and "\
            "#{second_year}.\n\n"
+      nil
     end
 
     # method to print additional information before the x and y
     # domain informations
-    def self.print_meta_head
+    def print_meta_head
       puts "\nDataset: Difference of #{@meta_data.name} between two datasets"
+      nil
     end
 
   end
