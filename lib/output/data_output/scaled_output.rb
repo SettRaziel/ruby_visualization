b393fd9009b2b9b7791fad3c87e659dba83bcdb5
Benjamin Held <me@bheld.eu>
2017-01-05 21:40:16 +0100

    cont. refactoring scaled_output

--- lib/output/data_output/scaled_output.rb
+++ lib/output/data_output/scaled_output.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-04-14 16:46:51
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-04-14 17:08:39
+# @Last Modified time: 2017-01-05 21:39:31
 
 module DataOutput
 
@@ -20,7 +20,7 @@ module DataOutput
     # @param [DataSet] data_set the data set which should be visualized
     # @param [MetaData] meta_data the corresponding meta data
     # @param [Hash] options hash with the relevant parameter values
-    def self.prepare_attributes(data_set, meta_data, options)
+    def prepare_attributes(data_set, meta_data, options)
       # create meta data and data set for the scaled output
       sc = DatasetScaling.new(meta_data, data_set)
       @meta_data = sc.scaled_meta
@@ -31,7 +31,7 @@ module DataOutput
     # creates a headline before printing the data set based on the values
     # of the z dimension
     # @param [Integer] index the number of the dataset
-    def self.print_output_head(index, meta_data)
+    def print_output_head(index, meta_data)
       puts "\nPrinting autoscaled dataset for %.2f" %
             (meta_data.domain_z.lower + (index * meta_data.domain_z.step))
       puts "\n"
