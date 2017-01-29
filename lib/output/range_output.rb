92ee05be63710bbad2ab0d385c3df37288ab653d
Benjamin Held <me@bheld.eu>
2017-01-29 09:17:52 +0100

    cont. refactoring range_output

--- lib/output/range_output.rb
+++ lib/output/range_output.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2015-09-18 17:05:41
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-09-08 19:08:13
+# @Last Modified time: 2017-01-29 09:12:54
 
 require_relative '../data/meta_data'
 require_relative '../data/data_series'
@@ -15,7 +15,7 @@ class RangeOutput
   # @param [Hash] parameters the required parameter
   # @param [Hash] options hash with the boolean values for extreme values and
   #   extended legend output
-  def self.print_ranged_data(meta_data, data_series, parameters, options)
+  def initialize(meta_data, data_series, parameters, options)
     check_range_parameters(meta_data, parameters)
     options[:index] = parameters[:lower]
 
@@ -26,23 +26,23 @@ class RangeOutput
     end
   end
 
-  private_class_method
+  private
+
   # method to check if the input parameters are valid
   # @param [MetaData] meta_data the metadata of the used data series
   # @param [Hash] indices a hash containing the indices :first and :second
   # @return [boolean] true, if the parameters are in the range of the meta data
-  def self.check_range_parameters(meta_data, indices)
+  def check_range_parameters(meta_data, indices)
     first_lesser_second?(indices)
 
     parameter_in_meta_range?(meta_data, indices)
   end
 
-  private_class_method
   # method to check if the first parameter is lesser than the second
   # @param [Hash] indices a hash containing the indices :first and :second
   # @return [boolean] true, if first < second
   # @raise [ArgumentError] if first >= second
-  def self.first_lesser_second?(indices)
+  def first_lesser_second?(indices)
     if (indices[:lower] >= indices[:upper])
       raise ArgumentError,
         ' Error: First parameter of -r is equal or greater than the second'.red
@@ -50,14 +50,13 @@ class RangeOutput
     return true
   end
 
-  private_class_method
   # method to check if the parameters are within the range specified by the
   # {MetaData::MetaData}
   # @param [MetaData] meta_data the metadata of the used data series
   # @param [Hash] indices a hash containing the indices :first and :second
   # @return [boolean] true, if both parameter are within the meta data range
   # @raise [ArgumentError] if (first < 0 | second > data size)
-  def self.parameter_in_meta_range?(meta_data, indices)
+  def parameter_in_meta_range?(meta_data, indices)
     size = meta_data.domain_z.number_of_values
 
     if (indices[:lower] < 0)
@@ -73,25 +72,22 @@ class RangeOutput
     return true
   end
 
-  private_class_method
   # method to determine which type of output should be used
   # @param [DataSeries] data_series the used data series
   # @param [MetaData] meta_data the metadata of the used data series
   # @param [Hash] options hash with the boolean values for extreme values and
-  def self.determine_output_resolution(data_series, meta_data, options)
+  def determine_output_resolution(data_series, meta_data, options)
     if (!options[:auto_scale])
-      DataOutput::SingleOutput.print_dataset(data_series, meta_data, options)
+      DataOutput::SingleOutput.new(data_series, meta_data, options)
     else
-      DataOutput::ScaledDatasetOutput.
-                  print_dataset(data_series, meta_data, options)
+      DataOutput::ScaledDatasetOutput.new(data_series, meta_data, options)
     end
   end
 
-  private_class_method
   # method to determine the art of visualization. Without -all or animation
   # speed = 0 the progress will be manual, speed > 0 determines speed in seconds
   # @param [Hash] parameters the required parameters parameter
-  def self.determine_animation(parameters)
+  def determine_animation(parameters)
     animation_speed = 0
     if (parameters[:all])
       animation_speed = Integer(parameters[:all])
