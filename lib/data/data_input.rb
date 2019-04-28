df8ffc6f88e35de0d14716d745e376259598627f
Benjamin Held <me@bheld.eu>
2019-04-28 19:45:08 +0200

    switched to ruby_utils filereader

--- lib/data/data_input.rb
+++ lib/data/data_input.rb
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2018-01-27 17:04:01
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-04-09 20:08:19
+# @Last Modified time: 2019-04-28 19:42:37
 
 module DataInput
 
@@ -12,6 +12,6 @@ module DataInput
   require_relative 'data_repository'
   require_relative 'data_series'
   require_relative 'data_set'
-  require_relative 'file_reader'
+  require_relative '../ruby_utils/data/file_reader'
 
 end  
