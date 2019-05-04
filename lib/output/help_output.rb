fddfb4a091011f5e5b2bd3a555d646de07b5d8fc
Benjamin Held <me@bheld.eu>
2019-05-04 12:38:29 +0200

    switched to utils help output

--- lib/output/help_output.rb
+++ lib/output/help_output.rb
@@ -1,46 +1,17 @@
 # @Author: Benjamin Held
 # @Date:   2015-07-25 12:17:16
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-04-09 20:10:21
+# @Last Modified time: 2019-05-04 12:37:40
 
-require_relative '../ruby_utils/string/string'
+require_relative '../ruby_utils/help/help_output'
 
 # Output class for help text
-class HelpOutput
-
-  # method to print the help text for the given parameter
-  # @param [Symbol] parameter provided parameter
-  # @raise [ArgumentError] a non existent parameter is provided
-  def self.print_help_for(parameter)
-    initialize_output if (@parameters == nil)
-    if (@parameters[parameter])
-      puts 'TerminalVis help:'.light_yellow + "\n#{@parameters[parameter]}"
-    elsif (parameter)
-      print_help
-    else
-      raise ArgumentError, "help entry for #{parameter} does not exist".red
-    end
-  end
+class HelpOutput < BasicHelpOutput
 
   private
 
-  # @return [Hash] hash which stores available parameters and their help text
-  attr_reader :parameters
-
-  # method to initialize the hash containing the help entries
-  def self.initialize_output
-    @parameters = Hash.new()
-    add_single_help_entries
-    add_one_argument_help_entries
-    add_two_argument_help_entries
-  end
-
   # method to specify and add the help entries with help text only
   def self.add_single_help_entries
-    add_simple_text(:help, ' -h, --help     ', 'show help text')
-    add_simple_text(:version,
-                    ' -v, --version  ',
-                    'prints the current version of the project')
     add_simple_text(:extreme,
                     ' -e, --extreme  ', 'marks the extreme values in a ' \
                     'dataset with ++ for a maximum and -- for a minimum, ' \
@@ -88,70 +59,15 @@ class HelpOutput
           '; creates a timeline for the given coordinate (x,y), coordinates ' \
           'not laying on the data point will be interpolated, excludes -a,' \
           ' -c, and -i')
-  end
-
-  # method to add a (key, value) pair to the parameter hash
-  # @param [Symbol] symbol the key
-  # @param [String] text the value containing a formatted string
-  def self.add_text(symbol, text)
-    @parameters[symbol] = text
-  end
-
-  # method to add a (key, value) pair where the value contains the help text
-  # @param [Symbol] symbol the key
-  # @param [String] argument the string part containing the argument
-  # @param [String] text the string part containing the description text
-  def self.add_simple_text(symbol, argument, text)
-    add_text(symbol, argument.light_blue.concat(text))
-  end
-
-  # method to add a (key, value) pair where the value contains help text
-  # with one argument
-  # @param [Symbol] symbol the key
-  # @param [String] argument the string part containing the argument
-  # @param [String] parameter the string part containing the required parameter
-  # @param [String] text the string part containing the description text
-  def self.add_single_argument_text(symbol, argument, parameter, text)
-    add_text(symbol, build_entry(argument.light_blue, 'argument:',
-                                        parameter, text))
-  end
-
-  # method to add a (key, value) pair where the value contains help text
-  # with two argument
-  # @param [Symbol] symbol the key
-  # @param [String] argument the string part containing the argument
-  # @param [String] parameters the string part containing the
-  #   required parameters
-  # @param [String] text the string part containing the description text
-  def self.add_dual_argument_text(symbol, argument, parameters, text)
-    add_text(symbol, build_entry(argument.light_blue, 'arguments:',
-                                        parameters, text))
-  end
-
-  # method to build the entry text when dealing with one ore more parameters
-  # @param [String] parameter the string part containing the required paramter
-  # @param [String] quantity string entry to reflect the number of parameters
-  # @param [String] argument the string part containing the argument
-  # @param [String] text the string part containing the description text
-  # @return [String] the complete string representing the help entry
-  def self.build_entry(parameter, quantity, argument, text)
-    parameter + quantity.red + argument.yellow + text
-  end
+  end 
 
   # method to print the default help text
-  def self.print_help
+  def self.print_help_head
     puts 'script usage:'.red + " ruby <script> [parameters] <filename>"
     puts 'help usage :'.green + "              ruby <script> (-h | --help)"
     puts 'help usage for parameter:'.green +
        " ruby <script> <parameter> (-h | --help)"
-    puts "\nTerminalVis help:".light_yellow
-
-    @parameters.each_value { |value|
-      puts value
-    }
-
-    print_invalid_combinations
-    print_configuration_parameter
+    puts "\n#{get_script_name} help:".light_yellow
   end
 
   # method to print the invalid parameter combinations
@@ -170,6 +86,11 @@ class HelpOutput
     puts 'Color legend:'.blue + ' extended informations about the intervals'
     puts 'Scaling:'.blue + ' automatic scaling of the output to the size of ' \
          'the calling terminal (atm standard dataset output, e.g. -i only)'
+  end
+
+  # method to set the name of the script project
+  def self.get_script_name
+      "TerminalVis"
   end
 
 end
