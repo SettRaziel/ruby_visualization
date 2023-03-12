7820426b9ffee932bb240dbe773d21fc3a04a72d
Benjamin Held <me@bheld.eu>
2023-03-12 17:06:57 +0100

    add test for extreme value output

--- spec/output/output_spec.rb
+++ spec/output/output_spec.rb
@@ -3,13 +3,27 @@ require "spec_helper"
 describe TerminalVis::Output do
 
   describe "#create_output" do
-    context "given an array with data values" do
-      it "create the data set object with the given values" do
+    context "given input parameters and a file" do
+      it "read the data and create to correct visual output" do
         TerminalVis::initialize_repositories(["--file", "./spec/files/test_data"])
         TerminalVis.determine_configuration_options
         meta_data = TerminalVis.create_metadata()
         input = File.read(File.join(DATA_ROOT,"create_output"))
-        puts input.inspect
+        expect { 
+          TerminalVis::Output.create_output(meta_data)
+        }.to output(input).to_stdout
+                    
+      end
+    end
+  end
+
+  describe "#create_output" do
+    context "given input parameters and a file" do
+      it "read the data and create to correct visual output with extreme values" do
+        TerminalVis::initialize_repositories(["-e", "--file", "./spec/files/test_data"])
+        TerminalVis.determine_configuration_options
+        meta_data = TerminalVis.create_metadata()
+        input = File.read(File.join(DATA_ROOT,"create_extreme"))
         expect { 
           TerminalVis::Output.create_output(meta_data)
         }.to output(input).to_stdout
